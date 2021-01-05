3878ff2c78f5b6a40fcb0c52908f62721902f99a
Will Hopkins <willhopkins44@gmail.com>
2021-01-05 03:49:48 -0500

    WIP mongodb implementation

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('./passportSetup.js');
 const app = express();
 const port = 3000;
 
+// Middlewares
+
 app.use(session({
     secret: 'sweetwater',
     // store: 'MemoryStore', CHANGE TO SOMETHING ELSE FOR PRODUCTION. Check express-session page
@@ -18,13 +20,29 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(express.static('src/client/src', {
     index: 'index.html'
 }));
 
+// MongoDB
+
+const MongoClient = require('mongodb').MongoClient;
+const mongoUser = process.env.mongodb_user;
+const mongoPassword = process.env.mongodb_password;
+const mongoDatabase = 'ww2-map-database';
+
+const uri = `mongodb+srv://${mongoUser}:${mongoPassword}@${mongoDatabase}.sw7iz.mongodb.net/Authentication?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.listen(port, () => {
     console.log('Initialized');
+    console.log(process.env);
 });
 
 app.get('/auth/steam', passport.authenticate('steam'),
